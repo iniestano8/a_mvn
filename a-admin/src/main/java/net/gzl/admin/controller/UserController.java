71070ea85b67882805c2989df27806563c997e94
iniestano8 <iniestano8@163.com>
2015-04-14 19:48:42 +0800

    15-04-14 company

--- a-admin/src/main/java/net/gzl/admin/controller/UserController.java
+++ a-admin/src/main/java/net/gzl/admin/controller/UserController.java
@@ -1,17 +1,28 @@
 package net.gzl.admin.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController {
 
-	@RequestMapping("/toIndex")
+	@RequestMapping("/")
 	public ModelAndView toIndex(ModelAndView mav){
 		
 		mav.setViewName("index");
+		return mav;
+	}
+	
+	@RequestMapping("/toInfoEdit")
+	public ModelAndView toInfoEdit(ModelAndView mav, HttpServletRequest request){
+		mav.getModel().put("user", getLoginUser(request));
+		mav.getModel().put("authes", getAuthesByLoginUser(request));
+		mav.getModel().put("title", "title");
+		mav.setViewName("/basicInfo/info-edit");
 		return mav;
 	}
 	
